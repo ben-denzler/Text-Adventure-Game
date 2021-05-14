0e69cf4e6ea4c669d5da0f564ca314fb3a95ef0a
jbeed33 <jbeed33@gmail.com>
2021-05-13 22:41:53 -0700

    Inventory class updated and tests are supported

--- header/inventory.hpp
+++ header/inventory.hpp
@@ -11,24 +11,16 @@ class Inventory : public baseItem {
     public:
         ~Inventory();
         Inventory() : baseItem("", "Inventory"), inventory() {}
-        Inventory(string n, string t) : baseItem(n, "Inventory") {}
+        Inventory(string n) : baseItem(n, "Inventory") {}
         virtual void display();
         void addItem(baseItem*);
-        void addToInventory(baseItem* item) {
-            inventory.push_back(item);
-        }
+        void addToInventory(baseItem* item) { inventory.push_back(item); }
         void removeItem(baseItem*);
 };
 
 Inventory::~Inventory() {
-    //delete inventory
-    
+    // Delete inventory
     for (int i = 0; i < inventory.size(); i++) {
-        if (inventory.at(i)->getType() == "Inventory") {
-            for (int j = 0; j < inventory.size(); j++) {
-                delete inventory.at(i);
-            }
-        }
         delete inventory.at(i);
     }
 }
@@ -60,7 +52,6 @@ void Inventory::addItem(baseItem* item)
     cout << "Error: Could not find item. You are a failure of a developer. Please try and code again :)" << endl;
     return;
 }
-
 
 void Inventory::removeItem(baseItem* itemToRemove) {
     for (unsigned int i = 0; i < inventory.size(); ++i) {
